e6ac3da51e1cb021110dc27bbf3e16d09906048f
Nayeon Kim <rosie1025@naver.com>
2025-12-27 16:03:24 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,9 @@
+import math
+
+def solution(numer1, denom1, numer2, denom2):
+    new_numer= numer1*denom2 +numer2*denom1
+    new_denom=denom1*denom2
+    
+    gcd = math.gcd(new_numer,new_denom)
+    
+    return [new_numer//gcd, new_denom //gcd]
